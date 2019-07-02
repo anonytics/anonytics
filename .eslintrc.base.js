0d599d8ab9f2dade2749ae1b8c474fcb385b13b8
Jeppe Reinhold <jeppereinhold@gmail.com>
2019-07-02 22:31:52 +0200

    add unicorn eslint

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -2,11 +2,12 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
+    'plugin:unicorn/recommended',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'unicorn'],
   parserOptions: {
     sourceType: 'module',
   },
